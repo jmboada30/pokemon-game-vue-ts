ca65a8943562b8728a85f0620212f120361c48e7
Joel M. Boada <jmboada30@gmail.com>
2022-08-15 09:53:24 -0400

    ya estamos usando el store en pokemonPage

--- src/pages/PokemonPage.ts
+++ src/pages/PokemonPage.ts
@@ -1,9 +1,10 @@
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 import PokemonPicture from '../components/PokemonPicture.vue';
 import PokemonOptions from '../components/PokemonOptions.vue';
 import getPokemonOptions from '../helpers/getPokemonOptions';
-import { Pokemon } from '../interfaces/pokemon';
+import { usePokemonStore } from '../stores/pokemonStore';
+import { storeToRefs } from 'pinia';
 
 export default defineComponent({
   name: 'PokemonPage',
@@ -12,37 +13,26 @@ export default defineComponent({
     PokemonPicture,
   },
   setup: () => {
-    const pokemonArr = ref<Pokemon[]>([]);
-    const pokemon = ref<Pokemon>();
-    const showPokemon = ref(false);
-    const showAnswer = ref(false);
-    const message = ref('');
+    const pokemonStore = usePokemonStore();
+    const { pokemonArr, pokemon, showPokemon, showAnswer, message } =
+      storeToRefs(pokemonStore);
 
     const mixPokemonArray = async () => {
-      pokemonArr.value = await getPokemonOptions();
+      pokemonStore.loadPokemons(await getPokemonOptions());
       const randomId = Math.floor(Math.random() * 4);
-      pokemon.value = pokemonArr.value[randomId];
+      pokemonStore.setHiddenPokemon(pokemonArr.value[randomId]);
     };
 
     const checkAnswer = (pokemonId: number) => {
       if (!pokemon.value) return;
 
-      showPokemon.value = true;
-      showAnswer.value = true;
-
-      if (pokemonId === pokemon.value.id) {
-        message.value = `Correcto, ${pokemon.value.name}`;
-      } else {
-        message.value = `Oops, era ${pokemon.value.name}`;
-      }
+      pokemonId === pokemon.value.id
+        ? pokemonStore.showPokemonAndAnswer(`Correcto, ${pokemon.value.name}`)
+        : pokemonStore.showPokemonAndAnswer(`Oops, era ${pokemon.value.name}`);
     };
 
     const newGame = () => {
-      pokemonArr.value = [];
-      pokemon.value = undefined;
-      showPokemon.value = false;
-      showAnswer.value = false;
-      message.value = '';
+      pokemonStore.clearState();
       mixPokemonArray();
     };
 
